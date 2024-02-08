99f955b0edb36a2f37a2d3703a6753ec5ad8523e
davidplay7 <davidk1k3k@gmail.com>
2024-02-08 11:55:37 -0300

    inicio de null Safety

---
+++ variaveis/variaveis.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var a: String = "Variavel que pode ser mudada"
+
+    var b = "Variavel sem tipo e mutavel"
+
+    val c: String = "Não pode ser mudada"
+
+    val d = "Variavel imutavel e sem tipo definido"
+
+    val e: Int //Variavel não inicializada
+
+    e = 1
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    
+    
+}
